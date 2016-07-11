c5a36abb49e8890dbbdbf0f9ec18eaa640aaaa70
George Nachman <gnachman@gmail.com>
2016-07-10 23:08:55 -0700

    Fix a bug where the prefs panel would open when changing the font. Issue 4903

--- sources/PreferencePanel.m
+++ sources/PreferencePanel.m
@@ -241,6 +241,7 @@ NSString *const kSessionProfileDidChange = @"kSessionProfileDidChange";
 #pragma mark - NSWindowController
 
 - (void)windowWillLoad {
+    DLog(@"Will load prefs panel from %@", [NSThread callStackSymbols]);
     // We finally set our autosave window frame name and restore the one from the user's defaults.
     [self setWindowFrameAutosaveName:@"Preferences"];
 }
